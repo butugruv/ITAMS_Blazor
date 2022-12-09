2047f402379a0901997059a7097a9b560338042d
Chris <butugruv@gmail.com>
2022-12-09 12:48:12 -1000

    a

--- BlazorApp/Models/DisplayDeviceModel.cs
+++ BlazorApp/Models/DisplayDeviceModel.cs
@@ -1,25 +1,24 @@
 ﻿using ITAMS_DAL.Models;
-using System.ComponentModel.DataAnnotations;
 
 namespace BlazorApp.Models
 {
     public class DisplayDeviceModel : IDeviceModel
     {
-        [Editable(false)]
+
         public int Id { get; set; }
-        [Required]
+
         public string DeviceName { get; set; }
         public string DeviceFunction { get; set; }
         public int DeviceTypeId { get; set; }
         public int ManufacturerId { get; set; }
         public string Poc { get; set; }
-        [Editable(false)]
+
         public DateTime CreatedDate { get; set; }
-        [Editable(false)]
+
         public string CreatedBy { get; set; }
-        [Editable(false)]
+
         public DateTime ModifiedDate { get; set; }
-        [Editable(false)]
+
         public string ModifiedBy { get; set; }
         public int LocationId { get; set; }
         public int LocationFloorId { get; set; }
