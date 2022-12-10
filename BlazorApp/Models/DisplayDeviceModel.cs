6e8a097eb1b9c81bbb74d8d1138d94812dd74970
Chris <butugruv@gmail.com>
2022-12-09 19:52:34 -1000

    a

--- BlazorApp/Models/DisplayDeviceModel.cs
+++ BlazorApp/Models/DisplayDeviceModel.cs
@@ -24,6 +24,6 @@ namespace BlazorApp.Models
         public int LocationFloorId { get; set; }
         public int LocationFloorRoomId { get; set; }
         public int RmfPackageId { get; set; }
-        public string model { get; set; }
+        public string Model { get; set; }
     }
 }
