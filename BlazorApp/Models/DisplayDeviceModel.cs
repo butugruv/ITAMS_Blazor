9d91c89595a5523f1743aadb6ee5d41857d22dd0
Chris <butugruv@gmail.com>
2022-12-11 12:29:47 -1000

    a

--- BlazorApp/Models/DisplayDeviceModel.cs
+++ BlazorApp/Models/DisplayDeviceModel.cs
@@ -5,32 +5,50 @@ namespace BlazorApp.Models
 {
     public class DisplayDeviceModel : IDeviceModel
     {
-
+        [Display(Name = "Device ID#")]
         public int Id { get; set; }
+
         [Required]
+        [Display(Name = "Device Name")]
         public string DeviceName { get; set; }
+
+        [Display(Name = "Device Function")]
         public string DeviceFunction { get; set; }
+
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Value must be greater than 0")]
         public int DeviceTypeId { get; set; }
+
         [Required]
         public int ManufacturerId { get; set; }
+
+        [Display(Name = "POC")]
         public string Poc { get; set; }
+
         [Required]
+        [Display(Name = "Created Date")]
         public DateTime CreatedDate { get; set; }
+
         [Required]
         public string CreatedBy { get; set; }
 
         public DateTime ModifiedDate { get; set; }
 
         public string ModifiedBy { get; set; }
+
         [Required]
+        [Display(Name = "Location")]
         public int LocationId { get; set; }
+
         [Required]
         public int LocationFloorId { get; set; }
+
         [Required]
         public int LocationFloorRoomId { get; set; }
+
         [Required]
         public int RmfPackageId { get; set; }
+
         [Required(AllowEmptyStrings = false)]
         public string Model { get; set; }
 
