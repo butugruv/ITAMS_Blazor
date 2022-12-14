2499754e012aa59b9700a9f7124444ae82d7aa2d
Chris <butugruv@gmail.com>
2022-12-13 15:39:11 -1000

    a

---
+++ BlazorApp/Pages/Devices.cs
@@ -0,0 +1,57 @@
+﻿using ITAMS_DAL.Models;
+using Telerik.Blazor;
+using Telerik.Blazor.Components;
+
+namespace BlazorApp.Pages
+{
+    public partial class Devices
+    {
+        private List<IDevicesWithLookupsModel> deviceList;
+        //private ObservableCollection<IDevicesWithLookupsModel> devicesListObserved;
+        public bool windowVisible = false;
+        private int deviceId;
+        public TelerikNotification Notification { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            deviceList = await deviceData.GetDevicesWithLookups();
+            //devicesListObserved = new ObservableCollection<IDevicesWithLookupsModel>(deviceList);
+        }
+
+        private void HandleEditButtonClick(IDevicesWithLookupsModel device)
+        {
+            //int id = (args.Item as IDevicesWithLookupsModel).Id; 
+            //navigationManager.NavigateTo($"Devices/DeviceForm/{id}");
+            deviceId = device.Id;
+            windowVisible = true;
+
+        }
+
+        private void HandleCreateButtonClick()
+        {
+            deviceId = 0;
+            windowVisible = true;
+
+        }
+
+        public async Task HandleOnSubmitted()
+        {
+            windowVisible = false;
+            deviceList = await deviceData.GetDevicesWithLookups();
+            ShowNotifications();
+
+        }
+
+        public void ShowNotifications()
+        {
+            var closeDelay = 1500;
+
+            Notification.Show(new NotificationModel()
+            {
+                Text = "Device successfully updated",
+                ThemeColor = ThemeConstants.Notification.ThemeColor.Success,
+                CloseAfter = closeDelay
+            });
+        }
+    }
+}
--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -25,7 +25,7 @@ namespace BlazorApp
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-            builder.Services.AddSingleton<WeatherForecastService>();
+            //builder.Services.AddSingleton<WeatherForecastService>();
             builder.Services.AddSingleton(new ConnectionStringData
             {
                 SqlConnectionName = "Default"
